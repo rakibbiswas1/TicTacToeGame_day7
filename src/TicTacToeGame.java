01a5cbcec77d3d49fb0ee792a55cbd246f4f43dd
rakibbiswas1 <rakibbiswas.official@gmail.com>
2022-08-28 11:11:24 +0530

    TicTacToe Board created

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,8 +1,16 @@
 public class TicTacToeGame {
 
     public static void main(String[] args) {
-
+        char[] board = createBoard();
         System.out.println("Welcome to TicTacToe program");
 
+    }
+    private static char[] createBoard() {
+        char[] board = new char[10];
+        for(int index=0;index<board.length;index++)
+        {
+            board[index] = ' ';
+        }
+        return board;
     }
 }
