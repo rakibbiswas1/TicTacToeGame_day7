d5c25338789d5101565fecc936995cbc2f362c80
rakibbiswas1 <rakibbiswas.official@gmail.com>
2022-08-28 11:25:29 +0530

    ADDED:Method to display the current board

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -6,6 +6,7 @@ public class TicTacToeGame {
         char userLetter = chooseUserLetter(userInput);
         char computerLetter = (userLetter == 'X') ? 'O' : 'X';
         char[] board = createBoard();
+        showBoard(board);
     }
     private static char[] createBoard() {
         char[] board = new char[10];
@@ -18,5 +19,12 @@ public class TicTacToeGame {
     private static char chooseUserLetter(Scanner userInput) {
         System.out.println("Choose your letter: ");
         return userInput.next().toUpperCase().charAt(0);
+    }
+    private static void showBoard(char[] board) {
+        System.out.println( board[0] + " | " + board[1] + " | " + board[2] );
+        System.out.println("----------");
+        System.out.println( board[3] + " | " + board[4] + " | " + board[5] );
+        System.out.println("----------");
+        System.out.println( board[6] + " | " + board[7] + " | " + board[8] );
     }
 }
